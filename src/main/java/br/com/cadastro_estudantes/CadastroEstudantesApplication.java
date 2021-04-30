037fcda71290d104d9d068cc0bf5ec71016edf72
hcbarros <hcb@a.recife.ifpe.edu.br>
2021-04-30 20:48:02 -0300

    commit

---
+++ src/main/java/br/com/cadastro_estudantes/CadastroEstudantesApplication.java
@@ -0,0 +1,13 @@
+package br.com.cadastro_estudantes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroEstudantesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroEstudantesApplication.class, args);
+	}
+
+}
