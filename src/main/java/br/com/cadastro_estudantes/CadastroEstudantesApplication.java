481c94dff1264c413e209064c8d71462ab2d1f13
hcbarros <hcb@a.recife.ifpe.edu.br>
2021-05-01 21:18:42 -0300

    commit

--- src/main/java/br/com/cadastro_estudantes/CadastroEstudantesApplication.java
+++ src/main/java/br/com/cadastro_estudantes/CadastroEstudantesApplication.java
@@ -2,6 +2,9 @@ package br.com.cadastro_estudantes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class CadastroEstudantesApplication {
@@ -10,4 +13,16 @@ public class CadastroEstudantesApplication {
 		SpringApplication.run(CadastroEstudantesApplication.class, args);
 	}
 
+	
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("*")
+						.allowedMethods("PUT", "DELETE", "GET", "POST");
+			}
+		};
+	}
 }
