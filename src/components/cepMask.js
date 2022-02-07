dbcd38d87440ff154c7b1b8ce040bf65d70d0b37
Henrique Caetano <hcb@a.recife.ifpe.edu.br>
2022-02-07 13:15:15 -0300

    Updates

--- src/components/cepMask.js
+++
@@ -1,8 +0,0 @@
-
-
-export const cepMask = async (evt) => {
-    let value = evt.target.value;
-    value = value.replace(/\D/g,"")
-                 .replace(/(\d{5})(\d)/,"$1-$2") 
-    evt.target.value = value;             
-}
--- src/components/numberMask.js
+++
@@ -1,8 +0,0 @@
-
-
-export const numberMask = (evt, serie) => {
-
-    let value = evt.target.value;
-    value = value.replace(serie ? /([^1-9])/g : /\D/g,"");
-    evt.target.value = value;             
-}
